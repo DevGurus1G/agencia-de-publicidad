272dd68b8cfca013742e32daac4e781b786ec7fc
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-11-07 08:52:49 +0100

    Creacion inicial del archivo para la db

---
+++ db/db_create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS 'usuarios' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    apellidos VARCHAR(255) NOT NULL,
+    rol VARCHAR(255) NOT NULL,
+    direccion_imagen VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS 'anuncios' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    precio VARCHAR(255) NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS 'imagenes_anuncios' (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    direccion_imagen VARCHAR(255) NOT NULL,
+    anuncio_id INTEGER NOT NULL,
+    FOREIGN KEY (anuncio_id) REFERENCES anuncios(id)
+)
