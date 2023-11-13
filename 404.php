031ff23f1e924abca5f1503be4cbd692e3439832
Jorge Aparicio <Jorge.aparicio@ikasle.egibide.org>
2023-11-13 14:11:15 +0100

    pagina 404

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php require 'views/components/header.php'; ?>
+
+<main>
+    <p class="hola">
+        hola
+        <span class="Error">
+            Error 404
+        </span>
+        &mdash; Esta pagina no existe &mdash;
+    </p>
+</main>
+
+<?php require 'views/components/footer.php'; ?>
+
+
