1d50358a0c0ca8ab68a7169604f43dcc7cd0cefc
Jorge Aparicio <Jorge.aparicio@ikasle.egibide.org>
2023-11-14 09:15:49 +0100

    Revision de pagina de error 404 + separacion parcial de default.css

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php require 'views/components/header.php'; ?>
 
 <main>
-    <p class="hola">
-        hola
-        <span class="Error">
+    <p class="mensaje">
+        Ups, algo ha salido mal...
+        <span class="error">
             Error 404
         </span>
         &mdash; Esta pagina no existe &mdash;
