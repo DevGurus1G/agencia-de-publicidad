ec0ae080b9747cbfe24000f01abfc20176b11158
Alex Duran <alex.duran@ikasle.egibide.org>
2023-11-23 09:21:34 +0100

    Enlaces de toda la tarejta home

--- views/components/anuncio.php
+++ views/components/anuncio.php
@@ -1,14 +1,11 @@
 <div class="anuncio-card">
-<?php  ?>
-<img src="data:image/jpeg;base64,<?= base64_encode(
-  $anuncio['primera_imagen']
-) ?>" alt="Foto del anuncio mostrado" />
-    <div class="anuncio-card-info">
-        <h2><?= $anuncio['titulo'] ?></h2>
-        <p><?= $anuncio['descripcion'] ?></p>
-    <!-- <span><i class="fa fa-user"></i> Publicado por: Admin</span> -->
-        <a href="/anuncio?id=<?= $anuncio['anuncio_id'] ?>">Publicado por 
-        <?= $anuncio['nombre_anunciante'] ?>
-        </a>
-    </div>
+    <a href="/anuncio?id=<?= $anuncio['anuncio_id'] ?>" class="enlace-anuncio-card">
+        <img src="data:image/jpeg;base64,<?= base64_encode($anuncio['primera_imagen']) ?>" alt="Foto del anuncio mostrado" />
+        <div class="anuncio-card-info">
+            <h2><?= $anuncio['titulo'] ?></h2>
+            <p><?= $anuncio['descripcion'] ?></p>
+        <!-- <span><i class="fa fa-user"></i> Publicado por: Admin</span> -->
+            <a href="/anunciante?id=<?= $anuncio['anunciante'] ?>"><span>Publicado por <?= $anuncio['nombre_anunciante'] ?></span></a>
+        </div>
+    </a>
 </div>
