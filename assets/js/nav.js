1f4a6e38f405c4401a27e90b1b169506ed8da32b
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-11-24 13:21:35 +0100

    Terminad cuadro de dialog de la cookie

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,26 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const modo = document.querySelector('.modo');
-  const menuToggle = document.querySelector('.menu-toggle');
-  const menuFilter = document.querySelector('.menu-filter');
-  const navMenu = document.querySelector('.navMenu');
-  const navFilter = document.querySelector('.navFilter');
+const modo = document.querySelector('.modo');
+const menuToggle = document.querySelector('.menu-toggle');
+const menuFilter = document.querySelector('.menu-filter');
+const navMenu = document.querySelector('.navMenu');
+const navFilter = document.querySelector('.navFilter');
 
-  menuToggle.addEventListener('click', function () {
-    navMenu.classList.toggle('show');
-    if (navFilter.classList.contains('show')) {
-      navFilter.classList.remove('show');
-    }
-  });
+menuToggle.addEventListener('click', function () {
+  navMenu.classList.toggle('show');
+  if (navFilter.classList.contains('show')) {
+    navFilter.classList.remove('show');
+  }
+});
 
-  menuFilter.addEventListener('click', function () {
-    navFilter.classList.toggle('show');
-    if (navMenu.classList.contains('show')) {
-      navMenu.classList.remove('show');
-    }
-  });
+menuFilter.addEventListener('click', function () {
+  navFilter.classList.toggle('show');
+  if (navMenu.classList.contains('show')) {
+    navMenu.classList.remove('show');
+  }
+});
 
-  modo.addEventListener('click', function () {
-    document.querySelector('body').classList.toggle('dark');
+modo.addEventListener('click', function () {
+  document.querySelector('body').classList.toggle('dark');
+  if (cookieAceptado()) {
+    console.log('KJHSAKJDH');
     let modo;
     let fechaExpiracion = new Date();
     // Añade 3 meses en milisegundos (3 meses * 30 días * 24 horas * 60 minutos * 60 segundos * 1000 milisegundos)
@@ -33,26 +34,61 @@ document.addEventListener('DOMContentLoaded', function () {
       : (modo = 'light');
     document.cookie =
       'modo=' + modo + '; expires=' + fechaExpiracion + '; path=/';
-  });
+  }
+});
 
-  // Para buscar
-  const searchFormInput = document.querySelector('#form-search input');
-  let searchTimeout;
+function cookieAceptado() {
+  let aceptado = localStorage.getItem('cookieAceptado');
+  return aceptado == true;
+}
 
-  searchFormInput.addEventListener('input', () => {
-    clearTimeout(searchTimeout);
-    searchTimeout = setTimeout(search, 1000);
-  });
+// Para buscar
+const searchFormInput = document.querySelector('#form-search input');
+let searchTimeout;
 
-  async function search() {
-    try {
-      const response = await fetch(
-        `/?search=${searchFormInput.value.toLowerCase().trim()}&desde_cliente`,
+searchFormInput.addEventListener('input', () => {
+  clearTimeout(searchTimeout);
+  searchTimeout = setTimeout(search, 1000);
+});
+
+async function search() {
+  try {
+    const response = await fetch(
+      `/?search=${searchFormInput.value.toLowerCase().trim()}&desde_cliente`,
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Respuesta:', data);
+      mostrarAnunciosBuscados(data);
+    } else {
+      console.error(
+        'Error en la respuesta del servidor:',
+        response.status,
+        response.statusText,
       );
+    }
+  } catch (error) {
+    console.error('Error al realizar la búsqueda:', error);
+  }
+}
+
+async function mostrarAnunciosBuscados(anuncios) {
+  let cadena = '';
+
+  for (const anuncio of anuncios) {
+    cadena += `<div class='anuncio-card'>`;
+
+    // Imagen
+    try {
+      const response = await fetch(`/?img=${anuncio.primera_imagen_id}`);
+
       if (response.ok) {
-        const data = await response.json();
-        console.log('Respuesta:', data);
-        mostrarAnunciosBuscados(data);
+        // const blob = await response.blob();
+        // console.log(blob);
+        // const imagenUrl = URL.createObjectURL(blob);
+        // cadena += `<img src="${imagenUrl}" alt="Foto del anuncio mostrado" />`;
+        const imgBase64 = await response.text();
+        cadena += `<img src="data:image/png;base64,${imgBase64}" alt="Prueba"/>`;
       } else {
         console.error(
           'Error en la respuesta del servidor:',
@@ -61,46 +97,16 @@ document.addEventListener('DOMContentLoaded', function () {
         );
       }
     } catch (error) {
-      console.error('Error al realizar la búsqueda:', error);
-    }
-  }
-
-  async function mostrarAnunciosBuscados(anuncios) {
-    let cadena = '';
-
-    for (const anuncio of anuncios) {
-      cadena += `<div class='anuncio-card'>`;
-
-      // Imagen
-      try {
-        const response = await fetch(`/?img=${anuncio.primera_imagen_id}`);
-
-        if (response.ok) {
-          // const blob = await response.blob();
-          // console.log(blob);
-          // const imagenUrl = URL.createObjectURL(blob);
-          // cadena += `<img src="${imagenUrl}" alt="Foto del anuncio mostrado" />`;
-          const imgBase64 = await response.text();
-          cadena += `<img src="data:image/png;base64,${imgBase64}" alt="Prueba"/>`;
-        } else {
-          console.error(
-            'Error en la respuesta del servidor:',
-            response.status,
-            response.statusText,
-          );
-        }
-      } catch (error) {
-        console.error('Error al cargar la imagen:', error);
-      }
-
-      // Contenido del anuncio
-      cadena += `<div class="anuncio-card-info">`;
-      cadena += `<h2>${anuncio.titulo}</h2>`;
-      cadena += `<p>${anuncio.descripcion}</p>`;
-      cadena += `</div>`;
-      cadena += `</div>`;
+      console.error('Error al cargar la imagen:', error);
     }
 
-    document.querySelector('main').innerHTML = cadena;
+    // Contenido del anuncio
+    cadena += `<div class="anuncio-card-info">`;
+    cadena += `<h2>${anuncio.titulo}</h2>`;
+    cadena += `<p>${anuncio.descripcion}</p>`;
+    cadena += `</div>`;
+    cadena += `</div>`;
   }
-});
+
+  document.querySelector('main').innerHTML = cadena;
+}
