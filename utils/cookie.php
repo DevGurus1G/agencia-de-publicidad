259301fa9e767f12baaaff43f95351fd920e219e
Alex Duran <alex.duran@ikasle.egibide.org>
2023-11-28 09:31:31 +0100

    Rama para archivos instalacion

--- utils/cookie.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Por defecto modo claro
-$modo;
-if(isset($_COOKIE['modo'])){
-    $modo = $_COOKIE['modo'];
-}else{
-    $modo = "light";
-}
--- views/chat.view.php
+++
@@ -1,8 +0,0 @@
-<?php include 'views/components/header.php'; ?>
-<main>
-    <? foreach ($chats as $chat): ?>
-        <? include 'views/components/chat_card.php'; ?>
-    <? endforeach; ?>
-</main>
-
-<?php include 'views/components/footer.php'; ?>
